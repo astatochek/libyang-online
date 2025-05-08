03e3923f6cb30f1de0d9eb48ca25d9b57baf8bea
astatochek <astatochek@gmail.com>
2025-05-08 12:09:46 +0300

    - fix wasm script

--- libyang/build_libyang.sh
+++ libyang/build_libyang.sh
@@ -2,25 +2,54 @@
 set -e # Exit immediately on error
 
 # 1. Setup Emscripten
-source ./emsdk/emsdk_env.sh
+if [ ! -d "emsdk" ]; then
+  echo "Cloning emsdk..."
+  git clone https://github.com/emscripten-core/emsdk.git
+else
+  echo "emsdk already exists, skipping clone"
+fi
+cd emsdk
+./emsdk install latest
+./emsdk activate latest
+source ./emsdk_env.sh
+cd ..
 
-# 2. Build and install PCRE2 properly
+# 2. Build PCRE2
+if [ ! -d "pcre2-10-42" ]; then
+  echo "Downloading PCRE2..."
+  wget https://github.com/PCRE2Project/pcre2/releases/download/pcre2-10.42/pcre2-10.42.tar.gz
+  tar xvf pcre2-10.42.tar.gz
+  rm pcre2-10.42.tar.gz
+else
+  echo "PCRE2 elready exists, skipping download"
+fi
 cd pcre2-10.42
-mkdir -p build
+if [ ! -d "build" ]; then
+  mkdir build
+fi
 cd build
-# Configure with explicit installation prefix
-emcmake cmake .. -DCMAKE_INSTALL_PREFIX=$(pwd)/../../pcre2-install \
-  -DCMAKE_BUILD_TYPE=Release \
+emcmake cmake .. -DCMAKE_BUILD_TYPE=Release \
   -DBUILD_SHARED_LIBS=OFF \
   -DPCRE2_BUILD_PCRE2_8=ON \
-  -DPCRE2_SUPPORT_UNICODE=ON
-emmake make -j4
+  -DPCRE2_SUPPORT_UNICODE=ON \
+  -DCMAKE_INSTALL_PREFIX=$(pwd)/../../pcre2-install
 emmake make install
 cd ../..
 
 # 3. Build libyang with absolute paths
+if [ ! -d "libyang" ]; then
+  echo "Cloning libyang..."
+  git clone https://github.com/CESNET/libyang.git
+else
+  echo "libyang already exists, skipping clone"
+fi
+
 cd libyang
-mkdir -p build
+
+if [ ! -d "build" ]; then
+  mkdir -p build
+fi
+
 cd build
 # Use absolute paths for PCRE2
 PCRE2_DIR=$(realpath ../../pcre2-install)
@@ -49,4 +78,4 @@ emcc validator.c \
   -s ALLOW_MEMORY_GROWTH=1 \
   -s EXPORTED_RUNTIME_METHODS='["ccall","UTF8ToString"]' \
   -s ASSERTIONS=1 \
-  -O3 -o validator.js
+  -O3 -o ../public/wasm/validator.js
